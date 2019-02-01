8b959c7585d03e542d1b203ad0159ce5230e65a3
Joseph Zachary <zachary@cs.utah.edu>
2019-02-01 15:06:15 -0700

    Add PS4bDevelopmentTests

---
+++ Spreadsheet/PS4bDevelopmentTests/DevelopmentTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dependencies;
+using System.Collections.Generic;
+using System;
+
+namespace PS4DevelopmentTests
+{
+    [TestClass]
+    public class DevelopmentTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Null1()
+        {
+            DependencyGraph d = new DependencyGraph();
+            d.AddDependency("a", null);
+        }
+
+        [TestMethod]
+        public void Copy1()
+        {
+            var d1 = new DependencyGraph();
+            var d2 = new DependencyGraph(d1);
+            Assert.AreEqual(0, d1.Size);
+            Assert.AreEqual(0, d2.Size);
+        }
+
+        [TestMethod]
+        public void Copy5()
+        {
+            var d1 = new DependencyGraph();
+            d1.AddDependency("a", "b");
+            d1.AddDependency("d", "e");
+            var d2 = new DependencyGraph(d1);
+            d1.AddDependency("a", "c");
+            d2.AddDependency("d", "f");
+            Assert.AreEqual(2, new List<string>(d1.GetDependents("a")).Count);
+            Assert.AreEqual(1, new List<string>(d1.GetDependents("d")).Count);
+            Assert.AreEqual(2, new List<string>(d2.GetDependents("d")).Count);
+            Assert.AreEqual(1, new List<string>(d2.GetDependents("a")).Count);
+        }
+    }
+}
