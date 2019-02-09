5b9f517e1b010e0ca55db6da494986d16b8645ff
JUSTIN FRANCIS <u1063296@umail.utah.edu>
2019-02-08 22:15:50 -0700

    v1.0, tested, working, needs commenting

--- Spreadsheet/PS4bDevelopmentTests/DevelopmentTests.cs
+++ Spreadsheet/PS4bDevelopmentTests/DevelopmentTests.cs
@@ -39,5 +39,157 @@ namespace PS4DevelopmentTests
             Assert.AreEqual(2, new List<string>(d2.GetDependents("d")).Count);
             Assert.AreEqual(1, new List<string>(d2.GetDependents("a")).Count);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods1()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.AddDependency(null, "x");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods2()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.GetDependees(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods3()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.GetDependents(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods4()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.HasDependees(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods5()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.HasDependents(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods6()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.RemoveDependency(null, "x");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods7()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.RemoveDependency("x", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods8()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.ReplaceDependees(null, new HashSet<string>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods9()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.ReplaceDependees("x", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods10()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.ReplaceDependents("x", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods11()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.ReplaceDependents(null, new HashSet<string>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods12()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.AddDependency("x", "a");
+            dg.AddDependency("a", "x");
+            HashSet<string> hs = new HashSet<string>();
+            hs.Add("q");
+            hs.Add(null);
+            dg.ReplaceDependents("x", hs);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullMethods13()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            dg.AddDependency("x", "a");
+            dg.AddDependency("a", "x");
+
+            HashSet<string> hs = new HashSet<string>();
+            hs.Add("q");
+            hs.Add(null);
+            dg.ReplaceDependees("a", hs);
+        }
+
+        [TestMethod]
+        public void ConstructorDGParam()
+        {
+            DependencyGraph dg1 = new DependencyGraph();
+            dg1.AddDependency("a", "z");
+            dg1.AddDependency("a", "y");
+            dg1.AddDependency("a", "x");
+            dg1.AddDependency("b", "a");
+            DependencyGraph dg2 = new DependencyGraph(dg1);
+            dg1.RemoveDependency("a", "z");
+            dg1.RemoveDependency("a", "y");
+            dg1.RemoveDependency("a", "x");
+            dg1.RemoveDependency("b", "a");
+
+            Assert.AreEqual(0, dg1.Size);
+            Assert.AreEqual(4, dg2.Size);
+            Assert.IsTrue(dg2.HasDependees("a"));
+            Assert.IsTrue(dg2.HasDependents("a"));
+        }
+
+        [TestMethod]
+        public void ConstructorDGParam1()
+        {
+            DependencyGraph dg1 = new DependencyGraph();
+            dg1.AddDependency("a", "z");
+            dg1.AddDependency("a", "y");
+            dg1.AddDependency("a", "x");
+            dg1.AddDependency("b", "a");
+            DependencyGraph dg2 = new DependencyGraph(dg1);
+            dg2.AddDependency("c", "k");
+
+            Assert.AreEqual(4, dg1.Size);
+            Assert.AreEqual(5, dg2.Size);
+            Assert.IsTrue(dg2.HasDependees("k"));
+            Assert.IsFalse(dg1.HasDependents("c"));
+        }
     }
 }
