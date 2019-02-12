c039c7062f6d6e5dfbf652da46bfac87449f2473
JUSTIN FRANCIS <u1063296@umail.utah.edu>
2019-02-12 13:56:30 -0700

    inital commit

---
+++ Spreadsheet/Spreadsheet/SpreadSheet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Formulas;
+
+namespace SS
+{
+    class Spreadsheet : AbstractSpreadsheet
+    {
+        private Dictionary<string, string> table;
+
+        public Spreadsheet()
+        {
+            table = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
+        /// value should be either a string, a double, or a Formula.
+        /// </summary>
+        public override object GetCellContents(string name)
+        {
+            if(name.Equals(null) || name.Equals("invalid"))
+            {
+                throw new InvalidNameException();
+            }
+            
+            return 
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
